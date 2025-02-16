70e330c992fc0326fdfcf0bd937229b99489afff
khem-07 <khem53580@gmail.com>
2025-02-16 12:32:38 +0000

    array

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,39 @@
+// arrays
+
+const myArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+const myHeros = ["Shaktiman", "Naagraj"]
+
+const myArr2 = new Array(11, 12, 13, 14 , 15)
+
+// console.log(myArr[1]);
+// console.log(myArr2);
+
+//Array methods
+
+
+// myArr.push(6)
+// myArr2.push(6)
+
+// myArr.pop()
+
+// myArr.unshift(10) //insert at the beggining of an array
+// myArr2.shift()    // remove the element from the begginning of an array
+
+// const newArr = myArr.join() // converts array type in string
+// console.log(myArr);
+// console.log(typeof newArr);
+
+
+// console.log(myArr2);
+
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log("B",myArr);
+
+const myn2 = myArr.splice(1, 3)
+console.log(myn1);
+console.log("c",myArr);
+
+
