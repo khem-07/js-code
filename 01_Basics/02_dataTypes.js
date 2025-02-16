4be6cee06df83f5cca1b6f3cbf00ecf03c147d1e
khem-07 <khem53580@gmail.com>
2025-02-16 09:00:40 +0000

    data types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) we are using nodejs, not browser
+
+console.log(3+3); 
+console.log("Khem");
+
+let name = "Khems"
+let age = 19
+let isloggedIn = false
+let state;
+
+// number => 2 to the power 53 range 
+// bigint => for large number
+// string => ""
+// boolesn => true/false
+// null => standalone value(representaion of empty value)
+// undefined => 
+// symbol => unique 
+
+//object 
+
+
+console.log(typeof "hitesh");
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
+
+
+
