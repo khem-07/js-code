e1b0ba9f97b82883b8b74d98c20de4b3c6a3ada7
khem-07 <khem53580@gmail.com>
2025-02-17 06:44:37 +0000

    one

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,38 @@
+// // if 
+// // const isUserLoggedIn = true
+// // if (2 === '2'){
+// //   console.log("executed"); 
+// // }
+// // else console.log("Not executed");
+
+// // const score = 200;
+
+// // if(score > 100){
+// //     const power = "fly"
+// //     console.log(`User power: ${power}`);
+    
+// // }
+// // console.log(`User power: ${power}`);
+
+// const balance = 1000 
+// //if(balance > 500) console.log("test");
+
+// if(balance < 500){
+// console.log("less than");
+// }
+// else if(balance < 750){
+//     console.log("less than 750"); 
+// }
+
+const userLoggedIn = true 
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard && 2==2){
+console.log("Allow to buy course");
+}
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User Logged In");
+}
+
