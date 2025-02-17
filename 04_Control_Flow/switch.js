bdc53522d9ed0cd9f0e18edf17c5b48ded1a6280
khem-07 <khem53580@gmail.com>
2025-02-17 06:50:46 +0000

    switch

---
+++ 04_Control_Flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "march":
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
