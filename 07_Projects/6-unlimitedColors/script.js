fdcb625024ff60292ae3f68416463c06ec46f541
khem-07 <khem53580@gmail.com>
2025-02-19 04:29:47 +0000

    randomcolor

---
+++ 07_Projects/6-unlimitedColors/script.js
@@ -0,0 +1,47 @@
+// generate a random color 
+
+// const randomColor = function(){
+//     const hex ="0123456789ABCDEF"
+//     let color = '#'
+//     for(let i = 0; i < 6; i++){
+//         color += hex[Math.floor(Math.random() * 16)]
+        
+//     } 
+//     return color;
+// };
+
+// const randomColor = function() {
+//     let h = Math.floor(Math.random() * 360); // Full spectrum
+//     let s = Math.floor(Math.random() * 40) + 60; // 60% - 100% saturation
+//     let l = Math.floor(Math.random() * 30) + 50; // 50% - 80% lightness
+
+//     return `hsl(${h}, ${s}%, ${l}%)`;
+// };
+
+
+
+
+let intervalId;
+
+const startChangingColor = function(){
+
+   if (!intervalId) {
+    intervalId = setInterval(changeBgColor, 1000);
+   }
+
+    function changeBgColor(){
+        document.body.style.backgroundColor = randomColor();
+    }
+};
+
+const stopChangingColor = function(){
+    clearInterval(intervalId);
+    intervalId = null;
+};
+
+document.querySelector("#start").addEventListener
+('click',startChangingColor)
+
+document.querySelector("#stop").addEventListener
+('click',stopChangingColor)
+
