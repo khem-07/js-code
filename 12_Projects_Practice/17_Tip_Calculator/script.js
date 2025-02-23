3756ea738e8739aecad19b079fec0a16297a3a71
khem-07 <khem53580@gmail.com>
2025-02-23 12:07:33 +0530

    Add files via upload

---
+++ 12_Projects_Practice/17_Tip_Calculator/script.js
@@ -0,0 +1,40 @@
+const generateBillButton = document.querySelector(".generate-bill");
+const billAmount = document.querySelector(".bill-amount");
+const discountPercentage = document.querySelector(".discount-percentage");
+const tipsPercentage = document.querySelector(".tip-percentage");
+const noOfCustomers = document.querySelector(".no-of-customers");
+const totalTipPaid = document.querySelector(".total-tip-paid");
+const totalAmountToPay = document.querySelector(".total-bill-amount-to-pay");
+const totalTipPercentageValue = document.querySelector(".discount-value");
+const discountPercentageValue = document.querySelector(".tip-value");
+const totalNoOfCustomerValue = document.querySelector(".no-of-customers-value");
+const eachCustomerToPay = document.querySelector('.each-customer-to-pay');
+
+
+
+
+function calculateBill() {
+  console.log(
+    billAmount.value,
+    tipsPercentage.value,
+    discountPercentage.value,
+    noOfCustomers.value
+  );
+
+  const billAmountAfterDiscountPercentage =
+    billAmount.value - (discountPercentage.value * billAmount.value) / 100;
+
+  const getTipAmount =
+    billAmountAfterDiscountPercentage * (tipsPercentage.value / 100);
+  const totalBill = billAmountAfterDiscountPercentage + getTipAmount;
+  
+  const eachCustomerToPayAmount = totalBill/ noOfCustomers.value;
+
+  totalTipPaid.textContent = getTipAmount;
+  totalAmountToPay.textContent = totalBill;
+  totalTipPercentageValue.textContent = tipsPercentage.value;
+  discountPercentageValue.textContent = discountPercentage.value;
+  totalNoOfCustomerValue.textContent = noOfCustomers.value;
+  eachCustomerToPay.textContent = eachCustomerToPayAmount;
+}
+generateBillButton.addEventListener("click", calculateBill);
