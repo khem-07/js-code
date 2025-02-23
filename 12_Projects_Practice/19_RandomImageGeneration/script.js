d594d9226d75420843100d1ed636242a291a6fd1
khem-07 <khem53580@gmail.com>
2025-02-23 14:35:35 +0530

    random image generation

---
+++ 12_Projects_Practice/19_RandomImageGeneration/script.js
@@ -0,0 +1,20 @@
+const imageWrapper = document.querySelector('.images-wrapper');
+const loadMoreImagesBtn = document.querySelector('.load-more-images');
+let count = 1;
+
+function fetchRandomImages(getCount){
+    for (let i = getCount; i <= getCount + 4; i++){
+        let imgElement = document.createElement('img');
+        imgElement.src = `https://picsum.photos/300?random=${i}`
+        imageWrapper.appendChild(imgElement);
+    }
+}
+
+
+
+ fetchRandomImages(count);
+
+
+ loadMoreImagesBtn.addEventListener('click',()=>{
+    fetchRandomImages((count += 6));
+ })
